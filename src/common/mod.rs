e615ca7f5e54136b2bf437f6f2f4a451c76e8635
Anson <git@anson-cheung.com>
2020-09-08 00:32:12 +0800

    Fixed line between

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -5,6 +5,7 @@ use std::convert::TryFrom;
 use crate::piece::piecetype::PieceType;
 use itertools::iproduct;
 use crate::board_representation::bitboard::files_ranks::FULL_BITBOARD;
+use crate::board_representation::square::ParseError::BitBoardNotUnit;
 
 pub mod moves;
 
@@ -38,8 +39,8 @@ pub static LINE_INTERSECTING: Lazy<[[BitBoard; 64]; 64]> = Lazy::new(|| {
 pub fn line_between(a: Square, b: Square) -> BitBoard {
     let b = LINE_INTERSECTING[u64::from(a) as usize][u64::from(b) as usize] &
         (
-            (FULL_BITBOARD << a.into()) ^
-            (FULL_BITBOARD << b.into())
+            (FULL_BITBOARD << u64::from(a).into()) ^
+            (FULL_BITBOARD << u64::from(b).into())
         );
     b & (b - 1.into())
 }
@@ -82,9 +83,9 @@ mod tests {
     }
 
     #[test]
-    fn test_line_between() {
-        let point_1: Square = 5_u64.try_into().unwrap();
-        let point_2: Square = 61_u64.try_into().unwrap();
+    fn line_between_rook() {
+        let point_1: Square = 10_u64.try_into().unwrap();
+        let point_2: Square = 58_u64.try_into().unwrap();
 
         println!("Point 1:{:?}", BitBoard::from(point_1));
         println!("Point 2:{:?}", BitBoard::from(point_2));
@@ -93,7 +94,22 @@ mod tests {
 
         println!("Between:{:?}", line_between);
 
-        assert_eq!(line_between, BitBoard::from(0x20202020202000));
+        assert_eq!(line_between, BitBoard::from(0x4040404040000));
+    }
+
+    #[test]
+    fn line_between_bishop() {
+        let point_1: Square = 18_u64.try_into().unwrap();
+        let point_2: Square = 54_u64.try_into().unwrap();
+
+        println!("Point 1:{:?}", BitBoard::from(point_1));
+        println!("Point 2:{:?}", BitBoard::from(point_2));
+
+        let line_between = line_between(point_1, point_2);
+
+        println!("Between:{:?}", line_between);
+
+        assert_eq!(line_between, BitBoard::from(0x201008000000));
     }
 
 }
